4d8196216f61c1732ac1b936e5a63a47096a93b0
AbhisheK <abhishek2001ckt@gmail.com>
2025-01-04 00:57:13 +0530

    file

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,17 +5,30 @@ const Home = () => {
     <>
       <div className="hero border-1 pb-3">
         <div className="card bg-dark text-white border-0 mx-3">
-                <img
-          className="card-img img-fluid"
-          src="./assets/main.jpg"
-          alt="Card"
-          style={{ width: '1800px', height: '600px' }}
-        />
-
-          
-          
+          <img
+            className="card-img img-fluid"
+            src="./assets/main.jpg"
+            alt="Card"
+            style={{
+              width: '100%',
+              height: 'auto',
+              maxHeight: '550px',
+              objectFit: 'cover', 
+            }}
+          />
         </div>
       </div>
+
+      <style jsx="true">
+        {`
+          @media (max-width: 768px) {
+            .card-img {
+              max-height: 300px; // Allow proportional resizing within a limit
+              object-fit: contain; // Prevent cropping
+            }
+          }
+        `}
+      </style>
     </>
   );
 };
